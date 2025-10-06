6a8f8403059b0042c7efeb75f7bdda10b9ed3753
DIVIhor <counter088@gmail.com>
2025-10-06 13:34:25 +0000

    feat: Translate the function usage for retrieving file information to the LLM and update the main function to enable the LLM to access functions from the available list

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,7 @@
 import os
 
+from google.genai import types
+
 
 def get_files_info(working_directory, directory="."):
     # output formatting
@@ -28,3 +30,20 @@ def get_files_info(working_directory, directory="."):
 
     except Exception as e:
         return f"{err_prefix} {e}"
+    
+
+# Tell the LLM how to use the `get_files_info` function
+# we won't allow the LLM to specify the `working_directory` parameter
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import schema_get_files_info
+
 
 def main():
     load_dotenv()
@@ -14,6 +16,22 @@ def main():
     # free tier model name
     model = "gemini-2.0-flash-001"
 
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
     # get prompt from CLI input
     if len(sys.argv) < 2:
         print("No prompt provided. Exiting...")
@@ -21,11 +39,28 @@ def main():
     user_prompt = sys.argv[1]
 
     # conversation history
-    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
-    resp = client.models.generate_content(model=model, contents=messages)
-    print(resp.text)
+    resp = client.models.generate_content(
+        model=model,
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
+    )
 
+    # show called functions with their used arguments
+    if resp.function_calls is not None:
+        for function_call in resp.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    # show the output
+    else:
+        print(resp.text)
+
+    # additional info
     if "--verbose" in sys.argv:
         # show the user's prompt
         print("User prompt:", user_prompt)
