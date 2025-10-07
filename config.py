8cdfb2210b451dddc7e9801f1db9e1664e033f2f
DIVIhor <counter088@gmail.com>
2025-10-07 18:48:10 +0000

    feat: Enable the model to act as an agent, iterating on its own outputs up to 20 times

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+# file contents character limit
 MAX_CHARS = 10000
+# max iterations by request to LLM
+MAX_ITERATIONS = 20
+# free-tier model name
+MODEL = "gemini-2.0-flash-001"
+# user's role
+ROLE = "user"
+# predefined woking directory
 WORKDIR = "./calculator"
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
 import sys
 
+from time import sleep
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
 from functions.call_function import available_functions, call_function
+
+from config import MAX_ITERATIONS, MODEL, ROLE
+from prompts import system_prompt
 
 
 def main():
@@ -13,42 +18,67 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     # create a new instance of a Gemini client
     client = genai.Client(api_key=api_key)
-    # free tier model name
-    model = "gemini-2.0-flash-001"
-
-    system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
 
     # get prompt from CLI input
     if len(sys.argv) < 2:
         print("No prompt provided. Exiting...")
         exit(1)
-    user_prompt = sys.argv[1]
+
+    # extract arguments
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
+
+    # populate a prompt string using provided arguments
+    user_prompt = " ".join(args)
+
+    # check for verbose info request
     is_verbose = "--verbose" in sys.argv
+    if is_verbose:
+        # show the user's prompt
+        print("User prompt:", user_prompt)
 
     # conversation history
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)])
-    ]
+    messages = [types.Content(role=ROLE, parts=[types.Part(text=user_prompt)])]
 
+    iters = 0
+    while iters <= MAX_ITERATIONS:
+        iters += 1
+        if iters == MAX_ITERATIONS:
+            print(f"Maximum iterations ({MAX_ITERATIONS}) reached.")
+            sys.exit(1)
+
+        try:
+            final_resp = generate_content(client, messages, is_verbose)
+            if final_resp:
+                print("Final response:")
+                print(final_resp)
+                break
+        except Exception as e:
+            print(f"Error generating content: {e}")
+
+
+def generate_content(client, messages, is_verbose):
     resp = client.models.generate_content(
-        model=model,
+        model=MODEL,
         contents=messages,
         config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
+            tools=[available_functions], system_instruction=system_prompt
         ),
     )
+
+    if is_verbose:
+        # show the number of prompt tokens
+        print("Prompt tokens:", resp.usage_metadata.prompt_token_count)
+        # show the number of response tokens
+        print("Response tokens:", resp.usage_metadata.candidates_token_count)
+
+    for candidate in resp.candidates:
+        messages.append(candidate.content)
+    # sleep(3)
 
     # show the output
     if not resp.function_calls:
@@ -56,35 +86,28 @@ All paths you provide should be relative to the working directory. You do not ne
 
     function_responses = []  # track responses
     # show called functions with their used arguments
-    if resp.function_calls is not None:
-        for function_call_part in resp.function_calls:
-            function_call_result = call_function(
-                function_call_part, is_verbose
+    for function_call_part in resp.function_calls:
+        function_call_result = call_function(function_call_part, is_verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception(
+                f"Empty function call result: {function_call_part.name}({function_call_part.args})"
             )
-            if (
-                not function_call_result.parts
-                or not function_call_result.parts[0].function_response
-            ):
-                raise Exception(
-                    f"No function responses generated for: {function_call_part.name}({function_call_part.args})"
-                )
-            if is_verbose:
-                print(
-                    f"-> {function_call_result.parts[0].function_response.response}"
-                )
-            function_responses.append(function_call_result.parts[0])
+        # show extended response
+        if is_verbose:
+            print(
+                f"-> {function_call_result.parts[0].function_response.response}"
+            )
+
+        function_responses.append(function_call_result.parts[0])
 
     if not function_responses:
-        raise Exception("no function responses generated, exiting.")
+        raise Exception("No function responses generated, exiting.")
 
-    # additional info
-    if is_verbose:
-        # show the user's prompt
-        print("User prompt:", user_prompt)
-        # show the number of prompt tokens
-        print("Prompt tokens:", resp.usage_metadata.prompt_token_count)
-        # show the number of response tokens
-        print("Response tokens:", resp.usage_metadata.candidates_token_count)
+    # save results to messages
+    messages.append(types.Content(parts=function_call_result.parts, role=ROLE))
 
 
 if __name__ == "__main__":
